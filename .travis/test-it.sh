e09cbc6cdcb2f0a5e08cc1cf3898e70d23301aa6
Martin Krasser <krasserm@googlemail.com>
2015-10-28 08:55:15 +0100

    Exclude all but one Cassandra test from running on Travis CI

---
+++ .travis/test-it.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sbt $1 "it:testOnly \
+  com.rbmhtechnology.eventuate.RecoverySpec \
+  com.rbmhtechnology.eventuate.crdt.* \
+  com.rbmhtechnology.eventuate.serializer.* \
+  com.rbmhtechnology.eventuate.snapshot.filesystem.* \
+  *Leveldb"
